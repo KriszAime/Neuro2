15993843ba4f9cc730bc5a125431d268630ca7b2
Elric Christopher <krisz0605@gmail.com>
2019-11-17 06:59:21 +0100

    Initial

--- Neuro2/NeuroNetwork.cpp
+++ Neuro2/NeuroNetwork.cpp
@@ -1,2 +1,13 @@
 #include "NeuroNetwork.h"
 
+bool NeuralNet::NeuralNetwork::AddHiddenLayer(int NumberOfNeurons)
+{
+	
+	return false;
+}
+
+bool NeuralNet::NeuralNetwork::TrainUntil(double AcceptedError, unsigned long MaxEpochs, bool IsComment)
+{
+
+	return false;
+}
--- Neuro2/NeuroNetwork.h
+++ Neuro2/NeuroNetwork.h
@@ -1,19 +1,46 @@
 #pragma once
 
 namespace NeuralNet {
-
-class NeuralNetwork
-{
-public:
-	NeuralNetwork()
+	struct NeuralPattern
 	{
-			
-	}
+		double* Inputs;
+		double* Outputs;
+	};
+	struct NeuralImportData
+	{
+		NeuralPattern* Patterns;
+		unsigned long PatternICount, PatternOCount, PatternCount;
+	};
+	struct NeuralHiddenLayer
+	{
+		double* Hidden; //node count
+		double** WeightIH; //Inputs Count * node Count
+		double* SumH; //weighted contribution from each input unit
+		int NumHidden; //<-node count
+	};
+	struct NeuralMemory
+	{
+		double* Input;
+		double* Output;
+		NeuralHiddenLayer* Layers;
+	};
+	class NeuralNetwork
+	{
+	public:
+		NeuralNetwork(NeuralImportData *TrainingData)
+		{
+			PatternCount = TrainingData->PatternCount;
+			NumInput = TrainingData->PatternICount;
+			NumOutput = TrainingData->PatternOCount;
+		}
 
-	bool AddHiddenLayer(int );
-
-private:
-
-};
+		bool AddHiddenLayer(int NumberOfNeurons);
+		bool TrainUntil(double AcceptedError, unsigned long MaxEpochs, bool IsComment); //Little number ex: 0.0004, Big Number ex: 99000
+		//bool Test
+	private:
+		NeuralMemory *Memory = nullptr; //Count = PatternCount
+		int PatternCount, NumInput, NumOutput;
+		int* ranpat=nullptr; //Pattern Futtatás sorrend
+	};
 
 }
